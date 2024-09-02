064fddb3471b09c1777d2ddcec37e9ed6506748d
ifshaulhaque <75359108+ifshaulhaque@users.noreply.github.com>
2024-09-02 11:15:31 +0530

    Initial Commit

---
+++ src/main/java/com/example/TeachNest/TeachNestApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeachNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TeachNest/TeachNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeachNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
